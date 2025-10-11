896cc7e9669bca8f9020cdf334947f6ae648c421
BayonetArch <sudipxd124@gmail.com>
2025-10-11 13:07:57 +0545

    Changed version

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ use std::{
     time::{Duration, Instant},
 };
 
-const VERSION: &'static str = "0.1.1";
+const VERSION: &'static str = "0.1.2";
 
 const JSON_PATH: &'static str = "/data/data/com.termux/files/home/.local/share";
 const JSON_NAME: &'static str = "pkgs.json";
